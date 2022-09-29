9b98ca376c733f77eeb4c348662a572547888678
Igor Pereira <igrpereira.dev@gmail.com>
2022-09-28 22:59:48 -0300

    alterando sistema de notas para input radio

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 let homePage = document.querySelector(".container");
 let thankYouPage = document.querySelector(".container2");
 let submit = document.querySelector(".submit");
-let ratingSeted = document.querySelectorAll(".rating-values");
+let ratingSeted = document.getElementsByName("rating-value");
 let ratingDisplay = document.querySelector(".rating-set");
 
 ratingSeted.forEach((element) => {
-  element.addEventListener("click", (event) => {
-    this.value = event.target.textContent;
+  element.addEventListener("click", () => {
+    this.value = element.getAttribute("value");
     if(value){
       Submit()
     }
   });
 });
 
-function Submit (){
+function Submit() {
   submit.addEventListener("click", () => {
     thankYouPage.classList.toggle("hide");
     homePage.classList.toggle("hide");
     ratingDisplay.innerHTML = `You selected out ${value} of 5`;
   });
 }
-
